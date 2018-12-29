408e4cc7b65fe4e355093ccd42f8a63efd547a2a
Monika Majstorovic <mmonnica@gmail.com>
2018-12-29 21:55:03 +0100

    Added themes: Farm Animals, Singers, Actors, Cities, Instruments, Sports.

--- MemoryWPF/MemoryWPF/DataHelpers/CardData.cs
+++ MemoryWPF/MemoryWPF/DataHelpers/CardData.cs
@@ -6,7 +6,14 @@ namespace MemoryWPF.DataHelpers
     public enum Theme
     {
         WildAnimals,
+        FarmAnimals,
         Math,
+        Actors,
+        Cities,
+        Instruments,
+        Singers,
+        Sports,
+
         // Add new themes here
     }
 
@@ -14,10 +21,10 @@ namespace MemoryWPF.DataHelpers
 
     public static class CardData
     {
-        public static Dictionary<Theme, SolidColorBrush> ThemeColors = new Dictionary<Theme, SolidColorBrush>();
-        public static Dictionary<Theme, List<string>> ThemeCardNamesEnglish = new Dictionary<Theme, List<string>>();
-        public static Dictionary<Theme, List<string>> ThemeCardNamesCroatian = new Dictionary<Theme, List<string>>();
-        public static Dictionary<Theme, List<string>> ThemeCardImages = new Dictionary<Theme, List<string>>();
+        public readonly static Dictionary<Theme, SolidColorBrush> ThemeColors = new Dictionary<Theme, SolidColorBrush>();
+        public readonly static Dictionary<Theme, List<string>> ThemeCardNamesEnglish = new Dictionary<Theme, List<string>>();
+        public readonly static Dictionary<Theme, List<string>> ThemeCardNamesCroatian = new Dictionary<Theme, List<string>>();
+        public readonly static Dictionary<Theme, List<string>> ThemeCardImages = new Dictionary<Theme, List<string>>();
 
         static CardData()
         {
@@ -26,6 +33,12 @@ namespace MemoryWPF.DataHelpers
 
             #region Colors
             ThemeColors[Theme.WildAnimals] = Brushes.Red;
+            ThemeColors[Theme.FarmAnimals] = Brushes.SaddleBrown;
+            ThemeColors[Theme.Actors] = Brushes.DarkRed;
+            ThemeColors[Theme.Cities] = Brushes.DarkOrange;
+            ThemeColors[Theme.Instruments] = Brushes.ForestGreen;
+            ThemeColors[Theme.Singers] = Brushes.Purple;
+            ThemeColors[Theme.Sports] = Brushes.CornflowerBlue;
             ThemeColors[Theme.Math] = Brushes.DarkBlue;
 
             // Add colors for new themes here 
@@ -83,6 +96,348 @@ namespace MemoryWPF.DataHelpers
                 path + theme + "wild_animals_penguin.jpg",
                 path + theme + "wild_animals_tiger.jpg",
                 path + theme + "wild_animals_zebra.jpg",
+
+
+            };
+            #endregion
+
+            #region FarmAnimals
+            theme = "Farm_Animals/";
+            ThemeCardNamesEnglish[Theme.FarmAnimals] = new List<string>()
+            {
+                "Cat",
+                "Chicken",
+                "Cow",
+                "Dog",
+                "Donkey",
+                "Duck",
+                "Goat",
+                "Goose",
+                "Horse",
+                "Pig",
+                "Rabbit",
+                "Sheep",
+                
+                // Add new animals here and don't forget the ','
+            };
+
+            ThemeCardNamesCroatian[Theme.FarmAnimals] = new List<string>()
+            {
+                "Mačka",
+                "Kokoš",
+                "Krava",
+                "Pas",
+                "Majmun",
+                "Patka",
+                "Koza",
+                "Guska",
+                "Konj",
+                "Svinja",
+                "Zec",
+                "Ovca",
+                
+                // Add new animals here and don't forget the ','
+            };
+
+            ThemeCardImages[Theme.FarmAnimals] = new List<string>()
+            {
+                path + theme + "farm_animals_cat.png",
+                path + theme + "farm_animals_chicken.jpg",
+                path + theme + "farm_animals_cow.png",
+                path + theme + "farm_animals_dog.png",
+                path + theme + "farm_animals_donkey.png",
+                path + theme + "farm_animals_duck.png",
+                path + theme + "farm_animals_goat.jpg",
+                path + theme + "farm_animals_goose.png",
+                path + theme + "farm_animals_horse.png",
+                path + theme + "farm_animals_pig.png",
+                path + theme + "farm_animals_rabbit.png",
+                path + theme + "farm_animals_sheep.png",
+
+
+            };
+            #endregion
+
+            #region Actors
+            theme = "Actors/";
+            ThemeCardNamesEnglish[Theme.Actors] = new List<string>()
+            {
+                "Angelina Jolie",
+                "Anne Hathaway",
+                "Brad Pitt",
+                "Christian Bale",
+                "Emma Watson",
+                "George Clooney",
+                "Jim Carrey",
+                "Julia Roberts",
+                "Leonardo Di Caprio",
+                "Natalie Portman",
+                "Rowan Atkinson",
+                "Tom Hanks",
+                
+                // Add new animals here and don't forget the ','
+            };
+
+            ThemeCardNamesCroatian[Theme.Actors] = new List<string>()
+            {
+                "Angelina Jolie",
+                "Anne Hathaway",
+                "Brad Pitt",
+                "Christian Bale",
+                "Emma Watson",
+                "George Clooney",
+                "Jim Carrey",
+                "Julia Roberts",
+                "Leonardo Di Caprio",
+                "Natalie Portman",
+                "Rowan Atkinson",
+                "Tom Hanks",
+                
+                // Add new animals here and don't forget the ','
+            };
+
+            ThemeCardImages[Theme.Actors] = new List<string>()
+            {
+                path + theme + "actors_angelinajolie.png",
+                path + theme + "actors_annehathaway.png",
+                path + theme + "actors_bradpitt.png",
+                path + theme + "actors_christianbale.png",
+                path + theme + "actors_emmawatson.png",
+                path + theme + "actors_georgeclooney.png",
+                path + theme + "actors_jimcarrey.png",
+                path + theme + "actors_juliaroberts.png",
+                path + theme + "actors_leonardodicaprio.png",
+                path + theme + "actors_natalieportman.png",
+                path + theme + "actors_rowanatkinson.png",
+                path + theme + "actors_tomhanks.png",
+
+
+            };
+            #endregion
+
+            #region Cities
+            theme = "Cities/";
+            ThemeCardNamesEnglish[Theme.Cities] = new List<string>()
+            {
+                "Agra",
+                "Barcelona",
+                "Dubai",
+                "London",
+                "Moscow",
+                "New York",
+                "Paris",
+                "Rio de Janero",
+                "Rome",
+                "San Francisco",
+                "Sydney",
+                "Zagreb",
+                
+                // Add new animals here and don't forget the ','
+            };
+
+            ThemeCardNamesCroatian[Theme.Cities] = new List<string>()
+            {
+                "Agra",
+                "Barcelona",
+                "Dubai",
+                "London",
+                "Moscow",
+                "New York",
+                "Paris",
+                "Rio de Janero",
+                "Rome",
+                "San Francisco",
+                "Sydney",
+                "Zagreb",
+                
+                // Add new animals here and don't forget the ','
+            };
+
+            ThemeCardImages[Theme.Cities] = new List<string>()
+            {
+                path + theme + "cities_agra.png",
+                path + theme + "cities_barcelona.png",
+                path + theme + "cities_dubai.png",
+                path + theme + "cities_london.jpg",
+                path + theme + "cities_moscow.png",
+                path + theme + "cities_newyork.png",
+                path + theme + "cities_paris.jpg",
+                path + theme + "cities_riodejanareo.png",
+                path + theme + "cities_rome.png",
+                path + theme + "cities_sanfrancisco.png",
+                path + theme + "cities_sydney.png",
+                path + theme + "cities_zagreb.jpg",
+
+
+            };
+            #endregion
+
+            #region Instruments
+            theme = "Instruments/";
+            ThemeCardNamesEnglish[Theme.Instruments] = new List<string>()
+            {
+                "Accordion",
+                "Cello",
+                "Clarinet",
+                "Drum",
+                "Flute",
+                "Guitar",
+                "Harp",
+                "Piano",
+                "Saxophone",
+                "Triangle",
+                "Trumpet",
+                "Violin",
+                
+                // Add new animals here and don't forget the ','
+            };
+
+            ThemeCardNamesCroatian[Theme.Instruments] = new List<string>()
+            {
+                "Harmonika",
+                "Violončelo",
+                "Klarinet",
+                "Bubanj",
+                "Flauta",
+                "Gitara",
+                "Harfa",
+                "Glasovir",
+                "Saksofon",
+                "Triangl",
+                "Truba",
+                "Violina",
+                
+                // Add new animals here and don't forget the ','
+            };
+
+            ThemeCardImages[Theme.Instruments] = new List<string>()
+            {
+                path + theme + "instruments_accordion.png",
+                path + theme + "instruments_cello.png",
+                path + theme + "instruments_clarinet.png",
+                path + theme + "instruments_drum.png",
+                path + theme + "instruments_flute.png",
+                path + theme + "instruments_guitar.png",
+                path + theme + "instruments_harp.png",
+                path + theme + "instruments_piano.png",
+                path + theme + "instruments_saxophone.png",
+                path + theme + "instruments_triangle.png",
+                path + theme + "instruments_trumpet.png",
+                path + theme + "instruments_violin.png",
+
+
+            };
+            #endregion
+
+            #region Singers
+            theme = "Singers/";
+            ThemeCardNamesEnglish[Theme.Singers] = new List<string>()
+            {
+                "Adele",
+                "Beyonce",
+                "Enrique Iglesias",
+                "Freddie Mercury",
+                "Justin Bieber",
+                "Justin Timberlake",
+                "Madonna",
+                "Michael Jackson",
+                "Rihanna",
+                "Selena Gomez",
+                "Shakira",
+                "Snoop Dogg",
+                
+                // Add new animals here and don't forget the ','
+            };
+
+            ThemeCardNamesCroatian[Theme.Singers] = new List<string>()
+            {
+                "Adele",
+                "Beyonce",
+                "Enrique Iglesias",
+                "Freddie Mercury",
+                "Justin Bieber",
+                "Justin Timberlake",
+                "Madonna",
+                "Michael Jackson",
+                "Rihanna",
+                "Selena Gomez",
+                "Shakira",
+                "Snoop Dogg",
+                
+                // Add new animals here and don't forget the ','
+            };
+
+            ThemeCardImages[Theme.Singers] = new List<string>()
+            {
+                path + theme + "singers_adele.png",
+                path + theme + "singers_beyonce.png",
+                path + theme + "singers_enriqueiglesias.png",
+                path + theme + "singers_freddiemercury.png",
+                path + theme + "singers_justinbieber.png",
+                path + theme + "singers_justintimberlake.png",
+                path + theme + "singers_madonna.png",
+                path + theme + "singers_michaeljackson.png",
+                path + theme + "singers_rihanna.png",
+                path + theme + "singers_selenagomez.png",
+                path + theme + "singers_shakira.png",
+                path + theme + "singers_snoopdogg.png",
+
+
+            };
+            #endregion
+
+            #region Sports
+            theme = "Sports/";
+            ThemeCardNamesEnglish[Theme.Sports] = new List<string>()
+            {
+                "Basketball",
+                "Boxing",
+                "Cycling",
+                "Football",
+                "Golf",
+                "High jump",
+                "Ice Skating",
+                "Running",
+                "Skiing",
+                "Tennis",
+                "Volleyball",
+                "Waterpolo",
+                
+                // Add new animals here and don't forget the ','
+            };
+
+            ThemeCardNamesCroatian[Theme.Sports] = new List<string>()
+            {
+                "Košarka",
+                "Boks",
+                "Biciklizam",
+                "Nogomet",
+                "Golf",
+                "Skok u vis",
+                "Klizanje",
+                "Trčanje",
+                "Skijanje",
+                "Tenis",
+                "Odbojka",
+                "Vaterpolo",
+                
+                // Add new animals here and don't forget the ','
+            };
+
+            ThemeCardImages[Theme.Sports] = new List<string>()
+            {
+                path + theme + "sports_basketball.png",
+                path + theme + "sports_boxing.png",
+                path + theme + "sports_cycling.png",
+                path + theme + "sports_football.png",
+                path + theme + "sports_golf.png",
+                path + theme + "sports_highjump.png",
+                path + theme + "sports_iceskating.png",
+                path + theme + "sports_running.png",
+                path + theme + "sports_skiing.png",
+                path + theme + "sports_tennis.png",
+                path + theme + "sports_volleyball.png",
+                path + theme + "sports_waterpolo.png",
 
 
             };
