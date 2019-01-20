c471132f25cd1fc8639e4aaf12a0028edbabe09e
Luka Valenta <valenta.luka@gmail.com>
2019-01-20 11:55:01 +0100

    Added language options for theme combobox.

---
+++ MemoryWPF/MemoryWPF/DataHelpers/DataObjectBase.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace MemoryWPF.DataHelpers
+{
+    public class DataObjectBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The event that is fired when any child property changes its value
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Call this to fire a <see cref="PropertyChanged"/> event
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ MemoryWPF/MemoryWPF/DataHelpers/ThemeObject.cs
@@ -0,0 +1,63 @@
+﻿namespace MemoryWPF.DataHelpers
+{
+    public class ThemeObject : DataObjectBase
+    {
+        #region Public Properties
+        private Theme theme;
+        public Theme Theme
+        {
+            get => theme;
+            set { theme = value; OnPropertyChanged("Theme"); }
+        }
+
+        public string English
+        {
+            get
+            {
+                switch (theme)
+                {
+                    case Theme.WildAnimals:
+                        return "Wild Animals";
+                    case Theme.FarmAnimals:
+                        return "Farm Animals";
+                    default:
+                        return theme.ToString();
+                }
+            }
+        }
+
+        public string Croatian
+        {
+            get
+            {
+                switch (theme)
+                {
+                    case Theme.WildAnimals:
+                        return "Divlje životinje";
+                    case Theme.FarmAnimals:
+                        return "Domaće životinje";
+                    case Theme.Math:
+                        return "Matematika";
+                    case Theme.Actors:
+                        return "Glumci";
+                    case Theme.Cities:
+                        return "Gradovi";
+                    case Theme.Instruments:
+                        return "Insturmenti";
+                    case Theme.Singers:
+                        return "Pjevači";
+                    case Theme.Sports:
+                        return "Sportovi";
+                    default:
+                        return "Divlje životinje";
+                }
+            }
+        }
+        #endregion
+
+        public ThemeObject(Theme theme)
+        {
+            this.theme = theme;
+        }
+    }
+}
